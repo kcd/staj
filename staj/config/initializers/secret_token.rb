f7a2d8c565f58ada480583f5df57354faab51048
nurdan tuğçe yeşilyurt <tugce.yesilyurt@bil.omu.edu.tr>
2012-11-01 18:34:00 +0200

    ilk

---
+++ staj/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staj::Application.config.secret_token = '03ae1edbfbb2eaa1a889d0211af2be3ea28ce7019ae06db28ce515df569d0d12f32c66f826e717d5d83188498bca1dc16daf137728cf44cba59e3bd8eb964eaa'
