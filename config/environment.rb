c0083afa98b25112fe3eb1d58f2bffc622edc259
Aydın Doyak <aydintd@bil.omu.edu.tr>
2012-11-16 01:59:19 +0200

    updates

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Staj::Application.initialize!
+
+AWS::S3::Base.establish_connection!(
+   :access_key_id     => ENV['S3_KEY'],
+   :secret_access_key => ENV['S3_SECRET']
+)
