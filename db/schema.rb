e6c899b229099f585cf20872fdcb7f43919b97b8
nurdan tuğçe yeşilyurt <tugce.yesilyurt@bil.omu.edu.tr>
2012-12-11 19:24:41 +0200

    db:migration hatası giderildi

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117214953) do
+ActiveRecord::Schema.define(:version => 20121202154526) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
@@ -24,8 +33,6 @@ ActiveRecord::Schema.define(:version => 20121117214953) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
